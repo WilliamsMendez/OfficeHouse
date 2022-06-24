f076cb4e593de50ae5fd68a9967def9be19efa0d
WilliamsMendez <93304351+WilliamsMendez88@users.noreply.github.com>
2022-06-24 16:07:46 -0700

    Borrar conexion de base de datos antigua
    SE borro conexion de base datos anterior, ya que genero errores

--- Cconexion.cs
+++ Cconexion.cs
@@ -8,34 +8,7 @@ using System.Windows.Forms;
 
 namespace OfficeHouse.clases
 {
-    /*internal class Cconexion
-    {
-        MySqlConnection conexion = new MySqlConnection();
-        static string servidor = "b5ixdlvobetayca5mkmt-mysql.services.clever-cloud.com";
-        static string db = "b5ixdlvobetayca5mkmt";
-        static string usuario = "u4rfifleiqqauwst";
-        static string password = "ZTTSrjjNnDg5BpwtsjeI";
-        static string puerto = "3306";
-
-        string cadeconexion = "server = " + servidor + ";" + "port = " + puerto + ";" + "user id = " + usuario + ";" + "password = " + password + ";" + "database =" + db + ";";
-
-        public MySqlConnection establecerconexion()
-        {
-            try {
-                conexion.ConnectionString = cadeconexion;
-                conexion.Open();
-                MessageBox.Show("Hubo conexion en la Base de Datos");
-            
-            }
-            catch (MySqlException)
-            {
-                MessageBox.Show("No hubo conexion");
-            
-            }
-            return conexion;
-        }
-    }*/
-
+    
     class Cconexion
     {
         public static MySqlConnection conex()
